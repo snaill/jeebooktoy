eb4ecf4cc66c60a5e34590bf8c2e0e4ad9262e00
com.chinaos.snaill <com.chinaos.snaill@f467227e-a901-11de-b6f4-e1be98fab775>
2009-09-25 15:27:54 +0000

    修改资源及对话框

--- JeebookToy/MainForm.Designer.cs
+++ JeebookToy/MainForm.Designer.cs
@@ -36,137 +36,139 @@ namespace JeebookToy
 		/// </summary>
 		private void InitializeComponent()
 		{
-			this.textBox1 = new System.Windows.Forms.TextBox();
-			this.button1 = new System.Windows.Forms.Button();
-			this.tabControl1 = new System.Windows.Forms.TabControl();
-			this.tabPage1 = new System.Windows.Forms.TabPage();
-			this.listView1 = new System.Windows.Forms.ListView();
-			this.tabPage2 = new System.Windows.Forms.TabPage();
-			this.listView2 = new System.Windows.Forms.ListView();
-			this.statusStrip1 = new System.Windows.Forms.StatusStrip();
-			this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
-			this.tabControl1.SuspendLayout();
-			this.tabPage1.SuspendLayout();
-			this.tabPage2.SuspendLayout();
-			this.flowLayoutPanel1.SuspendLayout();
-			this.SuspendLayout();
-			// 
-			// textBox1
-			// 
-			this.textBox1.Location = new System.Drawing.Point(49, 3);
-			this.textBox1.Name = "textBox1";
-			this.textBox1.Size = new System.Drawing.Size(328, 21);
-			this.textBox1.TabIndex = 0;
-			// 
-			// button1
-			// 
-			this.button1.Location = new System.Drawing.Point(3, 3);
-			this.button1.Name = "button1";
-			this.button1.Size = new System.Drawing.Size(40, 23);
-			this.button1.TabIndex = 1;
-			this.button1.Text = "Add";
-			this.button1.UseVisualStyleBackColor = true;
-			// 
-			// tabControl1
-			// 
-			this.tabControl1.Controls.Add(this.tabPage1);
-			this.tabControl1.Controls.Add(this.tabPage2);
-			this.tabControl1.Dock = System.Windows.Forms.DockStyle.Fill;
-			this.tabControl1.Location = new System.Drawing.Point(0, 27);
-			this.tabControl1.Name = "tabControl1";
-			this.tabControl1.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
-			this.tabControl1.RightToLeftLayout = true;
-			this.tabControl1.SelectedIndex = 0;
-			this.tabControl1.Size = new System.Drawing.Size(381, 217);
-			this.tabControl1.TabIndex = 2;
-			// 
-			// tabPage1
-			// 
-			this.tabPage1.Controls.Add(this.listView1);
-			this.tabPage1.Location = new System.Drawing.Point(4, 21);
-			this.tabPage1.Name = "tabPage1";
-			this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
-			this.tabPage1.Size = new System.Drawing.Size(373, 192);
-			this.tabPage1.TabIndex = 0;
-			this.tabPage1.Text = "tabPage1";
-			this.tabPage1.UseVisualStyleBackColor = true;
-			// 
-			// listView1
-			// 
-			this.listView1.Dock = System.Windows.Forms.DockStyle.Fill;
-			this.listView1.Location = new System.Drawing.Point(3, 3);
-			this.listView1.Name = "listView1";
-			this.listView1.Size = new System.Drawing.Size(367, 186);
-			this.listView1.TabIndex = 0;
-			this.listView1.UseCompatibleStateImageBehavior = false;
-			this.listView1.View = System.Windows.Forms.View.Details;
-			// 
-			// tabPage2
-			// 
-			this.tabPage2.Controls.Add(this.listView2);
-			this.tabPage2.Location = new System.Drawing.Point(4, 21);
-			this.tabPage2.Name = "tabPage2";
-			this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
-			this.tabPage2.Size = new System.Drawing.Size(373, 170);
-			this.tabPage2.TabIndex = 1;
-			this.tabPage2.Text = "tabPage2";
-			this.tabPage2.UseVisualStyleBackColor = true;
-			// 
-			// listView2
-			// 
-			this.listView2.Dock = System.Windows.Forms.DockStyle.Fill;
-			this.listView2.Location = new System.Drawing.Point(3, 3);
-			this.listView2.Name = "listView2";
-			this.listView2.Size = new System.Drawing.Size(367, 164);
-			this.listView2.TabIndex = 0;
-			this.listView2.UseCompatibleStateImageBehavior = false;
-			this.listView2.View = System.Windows.Forms.View.Details;
-			// 
-			// statusStrip1
-			// 
-			this.statusStrip1.Location = new System.Drawing.Point(0, 244);
-			this.statusStrip1.Name = "statusStrip1";
-			this.statusStrip1.Size = new System.Drawing.Size(381, 22);
-			this.statusStrip1.TabIndex = 3;
-			this.statusStrip1.Text = "statusStrip1";
-			// 
-			// flowLayoutPanel1
-			// 
-			this.flowLayoutPanel1.Controls.Add(this.button1);
-			this.flowLayoutPanel1.Controls.Add(this.textBox1);
-			this.flowLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Top;
-			this.flowLayoutPanel1.Location = new System.Drawing.Point(0, 0);
-			this.flowLayoutPanel1.Name = "flowLayoutPanel1";
-			this.flowLayoutPanel1.Size = new System.Drawing.Size(381, 27);
-			this.flowLayoutPanel1.TabIndex = 5;
-			// 
-			// MainForm
-			// 
-			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
-			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(381, 266);
-			this.Controls.Add(this.tabControl1);
-			this.Controls.Add(this.flowLayoutPanel1);
-			this.Controls.Add(this.statusStrip1);
-			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
-			this.Name = "MainForm";
-			this.Text = "JeebookToy";
-			this.tabControl1.ResumeLayout(false);
-			this.tabPage1.ResumeLayout(false);
-			this.tabPage2.ResumeLayout(false);
-			this.flowLayoutPanel1.ResumeLayout(false);
-			this.flowLayoutPanel1.PerformLayout();
-			this.ResumeLayout(false);
-			this.PerformLayout();
+            this.UrlTextBox = new System.Windows.Forms.TextBox();
+            this.AddButton = new System.Windows.Forms.Button();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.DownloadTabPage = new System.Windows.Forms.TabPage();
+            this.DownloadListView = new System.Windows.Forms.ListView();
+            this.FinishTabPage = new System.Windows.Forms.TabPage();
+            this.FinsihListView = new System.Windows.Forms.ListView();
+            this.StatusStrip = new System.Windows.Forms.StatusStrip();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.tabControl1.SuspendLayout();
+            this.DownloadTabPage.SuspendLayout();
+            this.FinishTabPage.SuspendLayout();
+            this.flowLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // UrlTextBox
+            // 
+            this.UrlTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.UrlTextBox.Location = new System.Drawing.Point(49, 3);
+            this.UrlTextBox.Name = "UrlTextBox";
+            this.UrlTextBox.Size = new System.Drawing.Size(328, 26);
+            this.UrlTextBox.TabIndex = 0;
+            // 
+            // AddButton
+            // 
+            this.AddButton.Location = new System.Drawing.Point(3, 3);
+            this.AddButton.Name = "AddButton";
+            this.AddButton.Size = new System.Drawing.Size(40, 25);
+            this.AddButton.TabIndex = 1;
+            this.AddButton.Text = "Add";
+            this.AddButton.UseVisualStyleBackColor = true;
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.DownloadTabPage);
+            this.tabControl1.Controls.Add(this.FinishTabPage);
+            this.tabControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tabControl1.Location = new System.Drawing.Point(0, 34);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.tabControl1.RightToLeftLayout = true;
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(381, 232);
+            this.tabControl1.TabIndex = 2;
+            // 
+            // DownloadTabPage
+            // 
+            this.DownloadTabPage.Controls.Add(this.DownloadListView);
+            this.DownloadTabPage.Location = new System.Drawing.Point(4, 22);
+            this.DownloadTabPage.Name = "DownloadTabPage";
+            this.DownloadTabPage.Padding = new System.Windows.Forms.Padding(3);
+            this.DownloadTabPage.Size = new System.Drawing.Size(373, 206);
+            this.DownloadTabPage.TabIndex = 0;
+            this.DownloadTabPage.Text = "Download";
+            this.DownloadTabPage.UseVisualStyleBackColor = true;
+            // 
+            // DownloadListView
+            // 
+            this.DownloadListView.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.DownloadListView.Location = new System.Drawing.Point(3, 3);
+            this.DownloadListView.Name = "DownloadListView";
+            this.DownloadListView.Size = new System.Drawing.Size(367, 200);
+            this.DownloadListView.TabIndex = 0;
+            this.DownloadListView.UseCompatibleStateImageBehavior = false;
+            this.DownloadListView.View = System.Windows.Forms.View.Details;
+            // 
+            // FinishTabPage
+            // 
+            this.FinishTabPage.Controls.Add(this.FinsihListView);
+            this.FinishTabPage.Location = new System.Drawing.Point(4, 22);
+            this.FinishTabPage.Name = "FinishTabPage";
+            this.FinishTabPage.Padding = new System.Windows.Forms.Padding(3);
+            this.FinishTabPage.Size = new System.Drawing.Size(373, 206);
+            this.FinishTabPage.TabIndex = 1;
+            this.FinishTabPage.Text = "Finish";
+            this.FinishTabPage.UseVisualStyleBackColor = true;
+            // 
+            // FinsihListView
+            // 
+            this.FinsihListView.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.FinsihListView.Location = new System.Drawing.Point(3, 3);
+            this.FinsihListView.Name = "FinsihListView";
+            this.FinsihListView.Size = new System.Drawing.Size(367, 200);
+            this.FinsihListView.TabIndex = 0;
+            this.FinsihListView.UseCompatibleStateImageBehavior = false;
+            this.FinsihListView.View = System.Windows.Forms.View.Details;
+            // 
+            // StatusStrip
+            // 
+            this.StatusStrip.Location = new System.Drawing.Point(0, 266);
+            this.StatusStrip.Name = "StatusStrip";
+            this.StatusStrip.Size = new System.Drawing.Size(381, 22);
+            this.StatusStrip.TabIndex = 3;
+            this.StatusStrip.Text = "StatusStrip";
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.Controls.Add(this.AddButton);
+            this.flowLayoutPanel1.Controls.Add(this.UrlTextBox);
+            this.flowLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(381, 34);
+            this.flowLayoutPanel1.TabIndex = 5;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(381, 288);
+            this.Controls.Add(this.tabControl1);
+            this.Controls.Add(this.flowLayoutPanel1);
+            this.Controls.Add(this.StatusStrip);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "MainForm";
+            this.Text = "JeebookToy";
+            this.tabControl1.ResumeLayout(false);
+            this.DownloadTabPage.ResumeLayout(false);
+            this.FinishTabPage.ResumeLayout(false);
+            this.flowLayoutPanel1.ResumeLayout(false);
+            this.flowLayoutPanel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
 		}
 		private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
-		private System.Windows.Forms.StatusStrip statusStrip1;
-		private System.Windows.Forms.ListView listView2;
-		private System.Windows.Forms.TabPage tabPage2;
-		private System.Windows.Forms.ListView listView1;
-		private System.Windows.Forms.TabPage tabPage1;
+		private System.Windows.Forms.StatusStrip StatusStrip;
+		private System.Windows.Forms.ListView FinsihListView;
+		private System.Windows.Forms.TabPage FinishTabPage;
+		private System.Windows.Forms.ListView DownloadListView;
+		private System.Windows.Forms.TabPage DownloadTabPage;
 		private System.Windows.Forms.TabControl tabControl1;
-		private System.Windows.Forms.Button button1;
-		private System.Windows.Forms.TextBox textBox1;
+		private System.Windows.Forms.Button AddButton;
+		private System.Windows.Forms.TextBox UrlTextBox;
 	}
 }
