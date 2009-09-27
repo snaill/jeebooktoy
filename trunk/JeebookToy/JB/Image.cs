7f69b34ea8aa4b163453970721fa77fc8b387061
com.chinaos.snaill <com.chinaos.snaill@f467227e-a901-11de-b6f4-e1be98fab775>
2009-09-27 05:21:59 +0000

    增加JB类定义

---
+++ trunk/JeebookToy/JB/Image.cs
@@ -0,0 +1,22 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: liming
+ * Date: 2009-9-27
+ * Time: 12:29
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace JeebookToy.JB
+{
+	/// <summary>
+	/// Description of Image.
+	/// </summary>
+	public class Image
+	{
+		public Image()
+		{
+		}
+	}
+}
---
+++ trunk/JeebookToy/JB/Para.cs
@@ -0,0 +1,22 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: liming
+ * Date: 2009-9-27
+ * Time: 12:23
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace JeebookToy.JB
+{
+	/// <summary>
+	/// Description of Para.
+	/// </summary>
+	public class Para
+	{
+		public Para()
+		{
+		}
+	}
+}
