59304dd78d6be5b0be8de762931a37bb0182ed3a
com.chinaos.snaill <com.chinaos.snaill@f467227e-a901-11de-b6f4-e1be98fab775>
2009-10-11 13:42:51 +0000

    修改界面

--- JeebookToy/MainForm.Designer.cs
+++ JeebookToy/MainForm.Designer.cs
@@ -38,29 +38,19 @@ namespace JeebookToy
 		{
 			this.components = new System.ComponentModel.Container();
 			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
-			this.UrlTextBox = new System.Windows.Forms.TextBox();
 			this.TaskListView = new System.Windows.Forms.ListView();
 			this.columnHeader1 = new System.Windows.Forms.ColumnHeader();
 			this.columnHeader2 = new System.Windows.Forms.ColumnHeader();
 			this.TaskStateImageList = new System.Windows.Forms.ImageList(this.components);
 			this.StatusStrip = new System.Windows.Forms.StatusStrip();
-			this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
-			this.AddButton = new JeebookToy.Controls.SplitButton();
-			this.SplitButtonMenuStrip = new System.Windows.Forms.ContextMenuStrip(this.components);
-			this.PluginTestMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-			this.EditorMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-			this.flowLayoutPanel1.SuspendLayout();
-			this.SplitButtonMenuStrip.SuspendLayout();
+			this.MainToolStrip = new System.Windows.Forms.ToolStrip();
+			this.AddToolStripButton = new System.Windows.Forms.ToolStripButton();
+			this.UrlTextBox = new JeebookToy.Controls.ToolStripSpringTextBox();
+			this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+			this.TesterToolButton = new System.Windows.Forms.ToolStripButton();
+			this.EditorToolButton = new System.Windows.Forms.ToolStripButton();
+			this.MainToolStrip.SuspendLayout();
 			this.SuspendLayout();
-			// 
-			// UrlTextBox
-			// 
-			this.UrlTextBox.Dock = System.Windows.Forms.DockStyle.Fill;
-			this.UrlTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-			this.UrlTextBox.Location = new System.Drawing.Point(3, 3);
-			this.UrlTextBox.Name = "UrlTextBox";
-			this.UrlTextBox.Size = new System.Drawing.Size(328, 26);
-			this.UrlTextBox.TabIndex = 0;
 			// 
 			// TaskListView
 			// 
@@ -69,9 +59,9 @@ namespace JeebookToy
 									this.columnHeader2});
 			this.TaskListView.Dock = System.Windows.Forms.DockStyle.Fill;
 			this.TaskListView.FullRowSelect = true;
-			this.TaskListView.Location = new System.Drawing.Point(0, 31);
+			this.TaskListView.Location = new System.Drawing.Point(0, 0);
 			this.TaskListView.Name = "TaskListView";
-			this.TaskListView.Size = new System.Drawing.Size(419, 218);
+			this.TaskListView.Size = new System.Drawing.Size(419, 272);
 			this.TaskListView.SmallImageList = this.TaskStateImageList;
 			this.TaskListView.TabIndex = 0;
 			this.TaskListView.UseCompatibleStateImageBehavior = false;
@@ -101,92 +91,98 @@ namespace JeebookToy
 			// 
 			// StatusStrip
 			// 
-			this.StatusStrip.Location = new System.Drawing.Point(0, 249);
+			this.StatusStrip.Location = new System.Drawing.Point(0, 272);
 			this.StatusStrip.Name = "StatusStrip";
 			this.StatusStrip.Size = new System.Drawing.Size(419, 22);
 			this.StatusStrip.TabIndex = 3;
 			this.StatusStrip.Text = "StatusStrip";
 			// 
-			// flowLayoutPanel1
+			// MainToolStrip
 			// 
-			this.flowLayoutPanel1.Controls.Add(this.UrlTextBox);
-			this.flowLayoutPanel1.Controls.Add(this.AddButton);
-			this.flowLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Top;
-			this.flowLayoutPanel1.Location = new System.Drawing.Point(0, 0);
-			this.flowLayoutPanel1.Name = "flowLayoutPanel1";
-			this.flowLayoutPanel1.Size = new System.Drawing.Size(419, 31);
-			this.flowLayoutPanel1.TabIndex = 5;
+			this.MainToolStrip.GripStyle = System.Windows.Forms.ToolStripGripStyle.Hidden;
+			this.MainToolStrip.ImageScalingSize = new System.Drawing.Size(32, 32);
+			this.MainToolStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+									this.AddToolStripButton,
+									this.UrlTextBox,
+									this.toolStripSeparator1,
+									this.TesterToolButton,
+									this.EditorToolButton});
+			this.MainToolStrip.LayoutStyle = System.Windows.Forms.ToolStripLayoutStyle.HorizontalStackWithOverflow;
+			this.MainToolStrip.Location = new System.Drawing.Point(0, 0);
+			this.MainToolStrip.Name = "MainToolStrip";
+			this.MainToolStrip.Size = new System.Drawing.Size(419, 39);
+			this.MainToolStrip.TabIndex = 6;
 			// 
-			// AddButton
+			// AddToolStripButton
 			// 
-			this.AddButton.ClickedImage = "Clicked";
-			this.AddButton.ContextMenuStrip = this.SplitButtonMenuStrip;
-			this.AddButton.DisabledImage = "Disabled";
-			this.AddButton.FocusedImage = "Focused";
-			this.AddButton.HoverImage = "Hover";
-			this.AddButton.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
-			this.AddButton.ImageKey = "Normal";
-			this.AddButton.Location = new System.Drawing.Point(337, 3);
-			this.AddButton.Name = "AddButton";
-			this.AddButton.NormalImage = "Normal";
-			this.AddButton.Size = new System.Drawing.Size(75, 21);
-			this.AddButton.TabIndex = 1;
-			this.AddButton.Text = "Add";
-			this.AddButton.TextImageRelation = System.Windows.Forms.TextImageRelation.TextBeforeImage;
-			this.AddButton.UseVisualStyleBackColor = true;
-			this.AddButton.ButtonClick += new System.EventHandler(this.AddButtonClick);
+			this.AddToolStripButton.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+			this.AddToolStripButton.Image = ((System.Drawing.Image)(resources.GetObject("AddToolStripButton.Image")));
+			this.AddToolStripButton.ImageTransparentColor = System.Drawing.Color.Magenta;
+			this.AddToolStripButton.Name = "AddToolStripButton";
+			this.AddToolStripButton.Size = new System.Drawing.Size(36, 36);
+			this.AddToolStripButton.Text = "Add";
+			this.AddToolStripButton.Click += new System.EventHandler(this.AddButtonClick);
 			// 
-			// SplitButtonMenuStrip
+			// UrlTextBox
 			// 
-			this.SplitButtonMenuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
-									this.PluginTestMenuItem,
-									this.EditorMenuItem});
-			this.SplitButtonMenuStrip.Name = "contextMenuStrip1";
-			this.SplitButtonMenuStrip.Size = new System.Drawing.Size(155, 70);
+			this.UrlTextBox.Name = "UrlTextBox";
+			this.UrlTextBox.Size = new System.Drawing.Size(271, 39);
 			// 
-			// PluginTestMenuItem
+			// toolStripSeparator1
 			// 
-			this.PluginTestMenuItem.Name = "PluginTestMenuItem";
-			this.PluginTestMenuItem.Size = new System.Drawing.Size(154, 22);
-			this.PluginTestMenuItem.Text = "Xslt Tester";
-			this.PluginTestMenuItem.Click += new System.EventHandler(this.PluginTestMenuItemClick);
+			this.toolStripSeparator1.Name = "toolStripSeparator1";
+			this.toolStripSeparator1.Size = new System.Drawing.Size(6, 39);
 			// 
-			// EditorMenuItem
+			// TesterToolButton
 			// 
-			this.EditorMenuItem.Name = "EditorMenuItem";
-			this.EditorMenuItem.Size = new System.Drawing.Size(154, 22);
-			this.EditorMenuItem.Text = "Jeebook Editor";
-			this.EditorMenuItem.Click += new System.EventHandler(this.EditorMenuItemClick);
+			this.TesterToolButton.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+			this.TesterToolButton.Image = ((System.Drawing.Image)(resources.GetObject("TesterToolButton.Image")));
+			this.TesterToolButton.ImageTransparentColor = System.Drawing.Color.Magenta;
+			this.TesterToolButton.Name = "TesterToolButton";
+			this.TesterToolButton.Size = new System.Drawing.Size(36, 36);
+			this.TesterToolButton.Text = "Xslt Tester";
+			this.TesterToolButton.ToolTipText = "Xslt Tester";
+			this.TesterToolButton.Click += new System.EventHandler(this.PluginTestMenuItemClick);
+			// 
+			// EditorToolButton
+			// 
+			this.EditorToolButton.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+			this.EditorToolButton.Image = ((System.Drawing.Image)(resources.GetObject("EditorToolButton.Image")));
+			this.EditorToolButton.ImageTransparentColor = System.Drawing.Color.Magenta;
+			this.EditorToolButton.Name = "EditorToolButton";
+			this.EditorToolButton.Size = new System.Drawing.Size(36, 36);
+			this.EditorToolButton.Text = "Jeebook Editor";
+			this.EditorToolButton.Click += new System.EventHandler(this.EditorMenuItemClick);
 			// 
 			// MainForm
 			// 
-			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(419, 271);
+			this.ClientSize = new System.Drawing.Size(419, 294);
+			this.Controls.Add(this.MainToolStrip);
 			this.Controls.Add(this.TaskListView);
-			this.Controls.Add(this.flowLayoutPanel1);
 			this.Controls.Add(this.StatusStrip);
-			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.SizableToolWindow;
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+			this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
 			this.Name = "MainForm";
 			this.Text = "JeebookToy";
 			this.Load += new System.EventHandler(this.MainFormLoad);
 			this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.MainFormFormClosed);
-			this.flowLayoutPanel1.ResumeLayout(false);
-			this.flowLayoutPanel1.PerformLayout();
-			this.SplitButtonMenuStrip.ResumeLayout(false);
+			this.MainToolStrip.ResumeLayout(false);
+			this.MainToolStrip.PerformLayout();
 			this.ResumeLayout(false);
 			this.PerformLayout();
 		}
-		private System.Windows.Forms.ToolStripMenuItem EditorMenuItem;
+		private System.Windows.Forms.ToolStrip MainToolStrip;
+		private System.Windows.Forms.ToolStripButton EditorToolButton;
+		private System.Windows.Forms.ToolStripButton TesterToolButton;
+		private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+		private System.Windows.Forms.ToolStripButton AddToolStripButton;
 		private System.Windows.Forms.ImageList TaskStateImageList;
 		private System.Windows.Forms.ListView TaskListView;
-		private System.Windows.Forms.ToolStripMenuItem PluginTestMenuItem;
-		private System.Windows.Forms.ContextMenuStrip SplitButtonMenuStrip;
-		private JeebookToy.Controls.SplitButton AddButton;
 		private System.Windows.Forms.ColumnHeader columnHeader2;
 		private System.Windows.Forms.ColumnHeader columnHeader1;
-		private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
 		private System.Windows.Forms.StatusStrip StatusStrip;
-		private System.Windows.Forms.TextBox UrlTextBox;
+		private JeebookToy.Controls.ToolStripSpringTextBox UrlTextBox;
 	}
 }
